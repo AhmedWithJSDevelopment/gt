1d3f64e42cdf140b78a956ac01b63abb7c820c3a
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2024-01-12 16:16:49 +0300

    Add files via upload

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,14 @@
+
+
+import {AudioPlayer} from '../components';
+
+import Audio from '../assets/Yani.mp3'
+
 const About = () => {
   return (
     <>
+            <AudioPlayer src={Audio} />
+
       <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
         <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
           We love
