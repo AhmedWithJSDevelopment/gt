a4e1cbef4a9c65d990a5e4205a520fe5af7580aa
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2026-04-13 00:58:46 +0300

    Switch production URL for API requests

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const productionUrl = ' https://strapi-store-server.onrender.com/api';
-const productionUrl = 'https://node-course-e-commerce-n20s.onrender.com/api/v1';
+const productionUrl = ' https://strapi-store-server.onrender.com/api';
 export const customFetch = axios.create({
   baseURL: productionUrl,
 });
