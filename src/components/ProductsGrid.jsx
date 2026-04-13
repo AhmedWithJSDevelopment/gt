e5c735d2b409c8c68163d4bc26e1cd17401417e6
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2026-04-13 14:34:28 +0300

    Refactor product attributes in ProductsGrid component

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -7,23 +7,23 @@ const ProductsGrid = () => {
   return (
     <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
       {products.map((product) => {
-        const { title, price, image } = product.attributes;
+        const { _id, name, price, image } = product;
         const dollarsAmount = formatPrice(price);
         return (
           <Link
-            key={product.id}
-            to={`/products/${product.id}`}
+            key={_id}
+            to={`/products/${_id}`}
             className='card w-full shadow-xl hover:shadow-2xl transition duration-300'
           >
             <figure className='px-4 pt-4'>
               <img
                 src={image}
-                alt={title}
+                alt={name}
                 className='rounded-xl h-64 md:h-48 w-full object-cover'
               />
             </figure>
             <div className='card-body items-center text-center'>
-              <h2 className='card-title capitalize tracking-wider'>{title}</h2>
+              <h2 className='card-title capitalize tracking-wider'>{name}</h2>
               <span className='text-secondary'>{dollarsAmount}</span>
             </div>
           </Link>
