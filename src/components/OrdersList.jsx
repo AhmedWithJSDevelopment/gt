8fe98e1104e944ec63e23457b3584331276447d8
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2026-04-13 15:45:38 +0300

    Refactor OrdersList to use count and orderItems

--- src/components/OrdersList.jsx
+++ src/components/OrdersList.jsx
@@ -4,37 +4,35 @@ import advancedFormat from 'dayjs/plugin/advancedFormat';
 day.extend(advancedFormat);
 
 const OrdersList = () => {
-  const { orders, meta } = useLoaderData();
+  // const { orders, meta } = useLoaderData();
+  const { orders, count } = useLoaderData();
+
 
   return (
     <div className='mt-8'>
       <h4 className='mb-4 capitalize'>
-        total orders : {meta.pagination.total}
+        total orders : {count}
       </h4>
       <div className='overflow-x-auto'>
         <table className='table table-zebra'>
           {/* head */}
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Address</th>
               <th>Products</th>
               <th>Cost</th>
+              <th>Date</th>
               <th className='hidden sm:block'>Date</th>
             </tr>
           </thead>
           <tbody>
             {orders.map((order) => {
               const id = order.id;
-              const { name, address, numItemsInCart, orderTotal, createdAt } =
-                order.attributes;
+               const { _id, total, createdAt, orderItems } = order;
               const date = day(createdAt).format('hh:mm a - MMM Do, YYYY');
               return (
-                <tr key={id}>
-                  <td>{name}</td>
-                  <td>{address}</td>
-                  <td>{numItemsInCart}</td>
-                  <td>{orderTotal}</td>
+                <tr key={_id}>
+                  <td>{orderItems.length}</td>
+                  <td>{total}</td>
                   <td className='hidden sm:block'>{date}</td>
                 </tr>
               );
