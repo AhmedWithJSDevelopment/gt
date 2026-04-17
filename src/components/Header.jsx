61ab932437eead9afa16737306e3b9d939bd7b08
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2026-04-17 08:46:51 +0300

    Change greeting from username to name in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
       <div className='align-element flex justify-center sm:justify-end'>
         {user ? (
           <div className='flex gap-x-2 sm:gap-x-8 items-center'>
-            <p className='text-xs sm:text-sm'>Hello, {user.username}</p>
+            <p className='text-xs sm:text-sm'>Hello, {user.name}</p>
             <button
               className='btn btn-xs btn-outline btn-primary'
               onClick={handleLogout}
