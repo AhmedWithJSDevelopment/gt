844a9a9962bca68936bdee5e475ed6c0027789f8
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2026-04-17 10:02:59 +0300

    Refactor loginUser to store user directly

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -31,10 +31,15 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    // loginUser: (state, action) => {
+    //   const user = { ...action.payload.user, token: action.payload.jwt };
+    //   state.user = user;
+    //   localStorage.setItem('user', JSON.stringify(user));
+
     loginUser: (state, action) => {
-      const user = { ...action.payload.user, token: action.payload.jwt };
-      state.user = user;
-      localStorage.setItem('user', JSON.stringify(user));
+  state.user = action.payload.user;
+  localStorage.setItem('user', JSON.stringify(action.payload.user));
+}
     },
     logoutUser: (state) => {
       state.user = null;
