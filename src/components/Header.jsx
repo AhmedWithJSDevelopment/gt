35dd5c24ac8732e64151e511da9105c3901ffbbc
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2026-04-17 10:46:24 +0300

    Refactor user selector in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const queryClient = useQueryClient();
-  // const user = useSelector((state) => state.user.name);
+  const user = useSelector((state) => state.user.user);
   // const user = useSelector((state) => state.user);
-  const user = useSelector((state) => state.userState.user.user);
+  // const user = useSelector((state) => state.userState.user.user);
        // console.log(state);
-    // console.log(user);
+    console.log(user);
   const handleLogout = () => {
     navigate('/');
     dispatch(clearCart());
